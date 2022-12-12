0140819021ebef3e1129fe9739dcbd6e106d78df
Adamcoakley <Adamcoakley18@gmail.com>
2022-12-12 10:34:02 +0000

    Updated comments

--- linux/loops.sh
+++ linux/loops.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# a file to make use of a basic array
+# a for loop is used to print the array's contents
+
 echo "Enter 3 names separated with spaces"
 read inputnames
 personnumber=1
--- linux/userinput.sh
+++ linux/userinput.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+'''
+a file that takes user input (a name)
+and returns: Hello + name
+'''
+
 echo "Please enter your name and press enter"
 read inputname
 
